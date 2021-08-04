f7ba20bcf6cc1061245c920685839bd9ed0e84be
[Thisara Hettikankanama] <[thisarayashodagcc@gmail.com]>
2021-08-04 16:12:50 +0530

    create blog page and articals

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Home from './pages/Home';
+import Article from './pages/Article';
+import AriticleList from './pages/AriticleList';
+import About from './pages/About';
 import {BrowserRouter as Router,Route} from 'react-router-dom';
 function App() {
   return (
     <Router>
         <div className='max-w-screen-md mx-auto pt-20'>
            <Route exact path='/' component={Home} />
+           <Route exact path='/about' component={About} />
+           <Route exact path='/articles-list' component={Article} />
+           <Route exact path='/article' component={AriticleList} />
         </div>
     </Router>
     
---
+++ src/pages/Article.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Article = () => {
+    return ( 
+        <>
+            <h1 className="sm:text-4xl text-2xl font-blod mt-6 mb-6 text-gray-900">
+                Articles
+            </h1>
+        </>
+     );
+}
+ 
+export default Article;
