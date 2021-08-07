f10ce302eb42a40b0739f598616399991d39c5bb
[Thisara Hettikankanama] <[thisarayashodagcc@gmail.com]>
2021-08-07 11:24:54 +0530

    add navgation bar

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import Article from './pages/Article';
 import AriticleList from './pages/AriticleList';
 import About from './pages/About';
 import {BrowserRouter as Router,Route} from 'react-router-dom';
+
+//component
+import NavBar from './componet/NavBar'
+
 function App() {
   return (
     <Router>
+      <NavBar/>
         <div className='max-w-screen-md mx-auto pt-20'>
            <Route exact path='/' component={Home} />
            <Route exact path='/about' component={About} />
